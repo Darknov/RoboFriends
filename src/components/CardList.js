3790c034246a60d99950a3d07d375515e0de86ed
Darknov <darknov.dono@gmail.com>
2018-06-11 21:02:42 +0200

    Add ErrorBoundary component

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,10 +2,15 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
+  if (true) {
+    throw new new Error('NOOOO!')();
+  }
   return (
     <div>
       {robots.map(({ id, name, email }, i) => {
-        return <Card key={i} id={id} name={name} email={email} />;
+        return (
+          <Card key={i} id={id} name={name} email={email} />
+        );
       })}
     </div>
   );
